2d93086a9074b19361174e105a39a7ffd23801eb
DESKTOP-MBRCU6K\Aleksandr <sk@vin.ua>
2025-12-30 19:26:57 +0200

    Add toggle switch for enabling/disabling model connection and update styles

--- src/components/apparate/Apparate.tsx
+++ src/components/apparate/Apparate.tsx
@@ -4,7 +4,6 @@ import { Sensor } from '../sensor'
 import { Sensor1 } from '../sensor1/Sensor1'
 import { Voltmeter } from '../voltmeter'
 import styles from './Apparate.module.css'
-import switchStyles from '../switch/Switch.module.scss'
 import { useApparateContext } from '../apparate'
 import { useTranslation } from "react-i18next"
 
@@ -12,57 +11,37 @@ export const Apparate = () => {
     const { t } = useTranslation()
     const { enabled, setEnabled, setVoltage } = useApparateContext()
 
-    const onToggle = (checked: boolean) => {
-        setEnabled(checked)
-        if (!checked) setVoltage(0)
+    const toggle = () => {
+        setEnabled(prev => {
+            const next = !prev
+            if (!next) setVoltage(0)
+            return next
+        })
     }
 
     return (
         <div className={styles.main}>
 
-            {/* SWITCH BOTTOM-RIGHT */}
+            {/* 🔘 ON / OFF BUTTON */}
             <div className={styles.switchBox}>
-                <input
-                    type="checkbox"
-                    id="connect-model"
-                    checked={enabled}
-                    onChange={(e) => onToggle(e.target.checked)}
-                    className={switchStyles.highload1}
-                />
-                <label
-                    htmlFor="connect-model"
-                    data-onlabel={t("connectModel.on")}
-                    data-offlabel={t("connectModel.off")}
-                    className={switchStyles.lb1}
-                />
+                <button
+                    className={`${styles.powerBtn} ${enabled ? styles.on : styles.off}`}
+                    onClick={toggle}
+                >
+                    {enabled ? 'ON' : 'OFF'}
+                </button>
+
                 <span className={styles.switchText}>
           {t("connectModel.title")}
         </span>
             </div>
 
-            <div className={styles.sensor}>
-                <Sensor1 text={t("model.strainGauge")} />
-            </div>
-
-            <div className={styles.sensor}>
-                <Sensor text={t("model.thermoSensor")} />
-            </div>
-
-            <div className={styles.sensor}>
-                <Voltmeter />
-            </div>
-
-            <div className={styles.sensor}>
-                <Sensor text={t("model.capacitiveSensor")} />
-            </div>
-
-            <div className={styles.sensor}>
-                <Photoresistor />
-            </div>
-
-            <div className={styles.sensor}>
-                <RadioBtn />
-            </div>
+            <div className={styles.sensor}><Sensor1 text={t("model.strainGauge")} /></div>
+            <div className={styles.sensor}><Sensor text={t("model.thermoSensor")} /></div>
+            <div className={styles.sensor}><Voltmeter /></div>
+            <div className={styles.sensor}><Sensor text={t("model.capacitiveSensor")} /></div>
+            <div className={styles.sensor}><Photoresistor /></div>
+            <div className={styles.sensor}><RadioBtn /></div>
 
         </div>
     )
